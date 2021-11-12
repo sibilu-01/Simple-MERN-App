60bb4be239cceda64dfb692939930c8e5506350d
sibilu-01 <sbaissa03@gmail.com>
2021-11-11 23:01:44 -0500

    Finished Redux setup and Connect to backend

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,38 @@
+import {v4 as uuid} from "uuid";
+import { ITEMS_LOADING, GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
+const initialState = {
+    items: [],
+    loading: false
+}
+
+export default function itemReducer(state = initialState, action ) {
+    switch (action.type) {
+        case GET_ITEMS:
+            console.log("payload is " + action.payload)
+            return {
+                ...state,
+                items: action.payload,
+                loading: false
+            }
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item._id !== action.payload)
+            }
+
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state
+    }
+}
+
